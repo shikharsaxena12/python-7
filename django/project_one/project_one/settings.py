7bf62870d960580d37cf2c94dcaaa0ac63ef5b60
Shikhar Saxena <shikharsaxena66625@gmail.com>
2026-02-25 21:59:31 +0530

    django setup completed

--- django/project_one/project_one/settings.py
+++ django/project_one/project_one/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'project_one_app'
+    'project_one_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
